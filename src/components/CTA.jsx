160716d8a2023cd37326b2197adf369e87d427f7
jeremy viterbo <viterbo1111@gmail.com>
2026-04-05 08:06:06 +0800

    cta

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -4,9 +4,9 @@ const CTA = () => {
   return (
     <section className='bg-primary py-6'>
         <div className="container flex flex-col gap-2 items-center text-center text-text">
-            <h4>Get in touch</h4>
+            <h2 className='text-2xl'>Get in touch</h2>
             <p>Let’s build innovative solutions together. Get in touch to discuss your project.</p>
-            <button className='btn bg-gray-700 text-white'>CONTACT</button>
+            <button className='btn bg-black text-white hover:bg-gray-700'>CONTACT</button>
         </div>
     </section>
   )
